05eb35d9e1f3c7be628fd7c3d232347dcefb603e
Nicolas Béguier <nicolas.beguier@adevinta.com>
2020-11-19 15:04:47 +0100

    Add context in print_subnet to simplify the subfunctions

--- libs/scan.py
+++ libs/scan.py
@@ -105,112 +105,135 @@ def remove_key_from_report(report, del_key, is_startswith=False):
         del report[key]
     return report
 
-def scan_mode(asset_report, asset_data, asset_name, security, brief=False):
+def scan_mode(report, context):
     """
     This functions is returning an asset_report with security findings,
     it handles the brief mode output
     """
     try:
         patterns = Patterns(
-            security['findings_rules_path'],
-            security['severity_levels'],
-            security['min_severity'],
-            security['max_severity']
+            context['security']['findings_rules_path'],
+            context['security']['severity_levels'],
+            context['security']['min_severity'],
+            context['security']['max_severity']
         )
     except Exception as err_msg:
         LOGGER.critical(err_msg)
         return []
-    security_issues = patterns.extract_findings(asset_data)
+    security_issues = patterns.extract_findings(report)
     if not security_issues:
         return []
-    asset_report['SecurityIssues'] = security_issues
-    if brief:
+    report['SecurityIssues'] = security_issues
+    if context['brief']:
         is_public = ''
-        if 'PubliclyAccessible' in asset_report and asset_report['PubliclyAccessible']:
+        if 'PubliclyAccessible' in report and report['PubliclyAccessible']:
             is_public = '[Public] '
-        asset_report = dict()
-        asset_report[f'{is_public}{asset_data["Type"]}: {asset_name}'] = \
+        report = dict()
+        report[f'{is_public}{context["asset_type"]}: {context["asset_name"]}'] = \
             [f['severity']+": "+f['title'] for f in security_issues]
-    return asset_report
+    return report
 
-def discover_mode(asset_report, asset_name, brief=False):
+def discover_mode(report, context):
     """
     This functions handles the brief mode output for discovery mode
     """
-    if brief:
+    if context['brief']:
         is_public = ''
-        if 'PubliclyAccessible' in asset_report and asset_report['PubliclyAccessible']:
+        if 'PubliclyAccessible' in report and report['PubliclyAccessible']:
             is_public = '[Public] '
-        return f'{is_public}{asset_report["Type"]}: {asset_name}'
-    return asset_report
+        return f'{is_public}{context["asset_type"]}: {context["asset_name"]}'
+    return report
 
-def update_report(report, asset_report, brief=False, verbose=False):
+def update_asset_report(new_report, report, context):
     """
-    This functions updates the current report by the given asset_report,
+    This functions updates the new report by the given report,
     it handles the brief and verbose output
     """
     # Keep SecurityGroups only in verbose mode
-    if not verbose and 'SecurityGroups' in asset_report:
-        del asset_report['SecurityGroups']
-    if brief:
-        report.append(asset_report)
+    if not context['verbose'] and 'SecurityGroups' in report:
+        del report['SecurityGroups']
+    if context['brief']:
+        new_report.append(report)
     else:
         # Put the asset_type between Subnet and Asset
-        asset_type = asset_report['Type']
-        if asset_type not in report:
-            report[asset_type] = list()
-        if 'Type' in asset_report:
-            del asset_report['Type']
-        report[asset_type].append(asset_report)
-    return report
+        if context['asset_type'] not in new_report:
+            new_report[context['asset_type']] = list()
+        if 'Type' in report:
+            del report['Type']
+        new_report[context['asset_type']].append(report)
+    return new_report
+
+def update_asset_type_report(new_report, report, context):
+    """
+    This functions updates the current report by the given asset_report
+    """
+    if context['asset_type'] not in context['meta_types']:
+        return new_report
+    vpc = context['vpc']
+    subnet = context['subnet']
+    subnet_slug = context['subnet_slug']
+    asset_type = context['asset_type']
+    meta_types = context['meta_types']
+    for asset in report[vpc]['Subnets'][subnet][asset_type]:
+        asset_report = report[vpc]['Subnets'][subnet][asset_type][asset]
+        context['asset'] = asset
+        context['asset_name'] = asset_report[meta_types[asset_type]['Name']]
+
+        if context['security']:
+            asset_report = scan_mode(
+                asset_report,
+                context)
+            if not asset_report:
+                return new_report
+        else:
+            asset_report = discover_mode(
+                asset_report,
+                context)
+
+        # Update the new report
+        new_report[vpc][subnet_slug] = update_asset_report(
+            new_report[vpc][subnet_slug],
+            asset_report,
+            context)
+    return new_report
 
 def print_subnet(report, meta_types, brief=False, verbose=False, security=None):
     """
     Print subnets
     """
     new_report = dict()
+    context = {
+        'vpc': None,
+        'subnet': None,
+        'subnet_slug': None,
+        'asset_type': None,
+        'asset': None,
+        'brief': brief,
+        'verbose': verbose,
+        'security': security,
+        'meta_types': meta_types
+        }
     for vpc in report:
+        context['vpc'] = vpc
         new_report[vpc] = dict()
         for subnet in report[vpc]['Subnets']:
-            mini_name = report[vpc]['Subnets'][subnet]['Name'].split(
+            context['subnet'] = subnet
+            context['subnet_slug'] = report[vpc]['Subnets'][subnet]['Name'].split(
                 f'-{report[vpc]["Subnets"][subnet]["AvailabilityZone"]}')[0]
-            if not mini_name in new_report[vpc]:
+            if not context['subnet_slug'] in new_report[vpc]:
                 if brief:
-                    new_report[vpc][mini_name] = list()
+                    new_report[vpc][context['subnet_slug']] = list()
                 else:
-                    new_report[vpc][mini_name] = dict()
-
+                    new_report[vpc][context['subnet_slug']] = dict()
             for asset_type in report[vpc]['Subnets'][subnet]:
-                if asset_type not in meta_types:
-                    continue
-                for asset in report[vpc]['Subnets'][subnet][asset_type]:
-                    asset_report = report[vpc]['Subnets'][subnet][asset_type][asset]
-
-                    if security:
-                        asset_report = scan_mode(
-                            asset_report,
-                            report[vpc]['Subnets'][subnet][asset_type][asset],
-                            asset_report[meta_types[asset_type]['Name']],
-                            security,
-                            brief=brief)
-                        if not asset_report:
-                            continue
-                    else:
-                        asset_report = discover_mode(
-                            asset_report,
-                            asset_report[meta_types[asset_type]['Name']],
-                            brief=brief)
-
-                    # Update the new report
-                    new_report[vpc][mini_name] = update_report(
-                        new_report[vpc][mini_name],
-                        asset_report,
-                        brief=brief,
-                        verbose=verbose)
-
+                context['asset_type'] = asset_type
+                new_report = update_asset_type_report(
+                    new_report,
+                    report,
+                    context)
             # Remove empty Subnet if brief mode
-            if brief and not new_report[vpc][mini_name]:
-                del new_report[vpc][mini_name]
+            if brief and not new_report[vpc][context['subnet_slug']]:
+                del new_report[vpc][context['subnet_slug']]
         # Remove empty VPC if brief mode
         if brief and not new_report[vpc]:
             del new_report[vpc]
