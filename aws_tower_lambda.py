bff5444ab1dbf2643309e41f6d84b372bbc9ed03
Nicolas Béguier <nicolas.beguier@adevinta.com>
2021-02-01 21:22:57 +0100

    Update version

--- aws_tower_lambda.py
+++ aws_tower_lambda.py
@@ -32,7 +32,7 @@ from config import variables
 # Debug
 # from pdb import set_trace as st
 
-VERSION = '2.6.2'
+VERSION = '2.7.0'
 
 PATROWL = dict()
 PATROWL['api_token'] = os.environ['PATROWL_APITOKEN']
