50e4b34fdf78ea2ff6b51683f2e73be7e9ff2a4c
Nicolas BEGUIER <nicolas_beguierwhotmail.com>
2024-03-13 10:02:02 +0100

    Release v4.6.0

--- config/variables.py
+++ config/variables.py
@@ -34,7 +34,7 @@ IAM_ROLENAME_PASSLIST = [
     'readonly'
 ]
 
-LAMBDA_SCAN_REGION_LIST = ['eu-west-1', 'eu-west-3']
+LAMBDA_SCAN_REGION_LIST = ['eu-west-1', 'eu-west-3', 'us-east-1', 'us-east-2', 'us-west-2', 'eu-central-1', 'ap-southeast-1']
 
 AWS_ALL_REGION_LIST = [
     'ap-northeast-1', 'ap-northeast-2', 'ap-northeast-3',
